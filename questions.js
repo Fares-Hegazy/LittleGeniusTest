9fe80949c69ad1098da04dd0b81985f43bb898b3
Fares-Hegazy <mhegazy1522@gmail.com>
2025-07-02 18:42:00 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 import { set1 } from './QuestionBank/set1.js';
-import { set2 } from './QuestionBank/set1.js';
+import { set2 } from './QuestionBank/set2.js';
 
 export const questionsBank = {
   "Set 1": set1,
