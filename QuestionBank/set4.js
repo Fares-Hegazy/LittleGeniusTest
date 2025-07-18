601b03dde0c588fae4759aa5d712cfe52a2766a5
Fares-Hegazy <mhegazy1522@gmail.com>
2025-07-18 17:52:25 +0300

    Create set4.js

---
+++ QuestionBank/set4.js
@@ -0,0 +1,13 @@
+export const set1 = [
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  4387</div>
+          <div class="row"><span class="op">+</span>  3237</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["7624", "7319", "7942", "7685"],
+    answer: "7624"
+  },
+  ];
