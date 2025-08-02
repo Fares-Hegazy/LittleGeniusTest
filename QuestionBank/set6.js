9db0a78ee0a8d6c58f09e9299d26c3716e0ff900
Fares-Hegazy <mhegazy1522@gmail.com>
2025-08-02 09:58:22 +0300

    Add files via upload

--- QuestionBank/set6.js
+++ QuestionBank/set6.js
@@ -2,7 +2,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>7 + 2 + 2 + 7 + 6 + 5 + 3 + 2 + 3 = </span>
+          <span><span class="number-term">7</span> + <span class="number-term">2</span> + <span class="number-term">2</span> + <span class="number-term">7</span> + <span class="number-term">6</span> + <span class="number-term">5</span> + <span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">3</span> = </span>
         </div>
       `,
     options: ["47", "27", "37", "57"],
@@ -11,7 +11,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>3 + 2 + 4 + 7 + 5 + 2 + 3 + 7 + 1 = </span>
+          <span><span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">4</span> + <span class="number-term">7</span> + <span class="number-term">5</span> + <span class="number-term">2</span> + <span class="number-term">3</span> + <span class="number-term">7</span> + <span class="number-term">1</span> = </span>
         </div>
       `,
     options: ["24", "34", "44", "54"],
@@ -20,7 +20,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>3 + 6 + 3 + 1 + 2 + 4 + 6 + 8 + 5 = </span>
+          <span><span class="number-term">3</span> + <span class="number-term">6</span> + <span class="number-term">3</span> + <span class="number-term">1</span> + <span class="number-term">2</span> + <span class="number-term">4</span> + <span class="number-term">6</span> + <span class="number-term">8</span> + <span class="number-term">5</span> = </span>
         </div>
       `,
     options: ["38", "28", "48", "58"],
@@ -29,7 +29,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>4 + 5 + 6 + 1 + 8 + 3 + 4 + 5 + 3 = </span>
+          <span><span class="number-term">4</span> + <span class="number-term">5</span> + <span class="number-term">6</span> + <span class="number-term">1</span> + <span class="number-term">8</span> + <span class="number-term">3</span> + <span class="number-term">4</span> + <span class="number-term">5</span> + <span class="number-term">3</span> = </span>
         </div>
       `,
     options: ["29", "49", "59", "39"],
@@ -38,7 +38,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>3 + 2 + 5 + 8 + 1 + 5 + 7 + 1 + 7 = </span>
+          <span><span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">5</span> + <span class="number-term">8</span> + <span class="number-term">1</span> + <span class="number-term">5</span> + <span class="number-term">7</span> + <span class="number-term">1</span> + <span class="number-term">7</span> = </span>
         </div>
       `,
     options: ["29", "49", "59", "39"],
@@ -47,7 +47,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>6 + 4 + 5 + 4 + 3 + 1 + 5 + 2 + 9 = </span>
+          <span><span class="number-term">6</span> + <span class="number-term">4</span> + <span class="number-term">5</span> + <span class="number-term">4</span> + <span class="number-term">3</span> + <span class="number-term">1</span> + <span class="number-term">5</span> + <span class="number-term">2</span> + <span class="number-term">9</span> = </span>
         </div>
       `,
     options: ["39", "49", "29", "59"],
@@ -56,7 +56,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>5 + 2 + 6 + 2 + 3 + 5 + 4 + 3 + 5 = </span>
+          <span><span class="number-term">5</span> + <span class="number-term">2</span> + <span class="number-term">6</span> + <span class="number-term">2</span> + <span class="number-term">3</span> + <span class="number-term">5</span> + <span class="number-term">4</span> + <span class="number-term">3</span> + <span class="number-term">5</span> = </span>
         </div>
       `,
     options: ["55", "45", "35", "25"],
@@ -65,7 +65,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>1 + 3 + 2 + 4 + 7 + 5 + 3 + 8 + 9 = </span>
+          <span><span class="number-term">1</span> + <span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">4</span> + <span class="number-term">7</span> + <span class="number-term">5</span> + <span class="number-term">3</span> + <span class="number-term">8</span> + <span class="number-term">9</span> = </span>
         </div>
       `,
     options: ["32", "42", "52", "62"],
@@ -74,7 +74,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>8 + 3 + 2 + 2 + 3 + 5 + 7 + 3 + 2 = </span>
+          <span><span class="number-term">8</span> + <span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">2</span> + <span class="number-term">3</span> + <span class="number-term">5</span> + <span class="number-term">7</span> + <span class="number-term">3</span> + <span class="number-term">2</span> = </span>
         </div>
       `,
     options: ["35", "25", "45", "55"],
@@ -83,7 +83,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>3 + 2 + 7 + 2 + 3 + 5 + 3 + 8 + 5 = </span>
+          <span><span class="number-term">3</span> + <span class="number-term">2</span> + <span class="number-term">7</span> + <span class="number-term">2</span> + <span class="number-term">3</span> + <span class="number-term">5</span> + <span class="number-term">3</span> + <span class="number-term">8</span> + <span class="number-term">5</span> = </span>
         </div>
       `,
     options: ["28", "48", "38", "58"],
@@ -92,7 +92,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>2 + 7 + 3 + 8 + 5 + 2 + 9 + 1 + 2 = </span>
+          <span><span class="number-term">2</span> + <span class="number-term">7</span> + <span class="number-term">3</span> + <span class="number-term">8</span> + <span class="number-term">5</span> + <span class="number-term">2</span> + <span class="number-term">9</span> + <span class="number-term">1</span> + <span class="number-term">2</span> = </span>
         </div>
       `,
     options: ["29", "39", "49", "59"],
@@ -101,7 +101,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>715 + 716 + 717 + 718 + 719 + 720 + 721 + 722 + 723 + 724 = </span>
+          <span><span class="number-term">715</span> + <span class="number-term">716</span> + <span class="number-term">717</span> + <span class="number-term">718</span> + <span class="number-term">719</span> + <span class="number-term">720</span> + <span class="number-term">721</span> + <span class="number-term">722</span> + <span class="number-term">723</span> + <span class="number-term">724</span> = </span>
         </div>
       `,
     options: ["7175", "7195", "7215", "7235"],
@@ -110,7 +110,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>922 + 923 + 924 + 925 + 926 + 927 + 928 + 929 + 930 + 931 = </span>
+          <span><span class="number-term">922</span> + <span class="number-term">923</span> + <span class="number-term">924</span> + <span class="number-term">925</span> + <span class="number-term">926</span> + <span class="number-term">927</span> + <span class="number-term">928</span> + <span class="number-term">929</span> + <span class="number-term">930</span> + <span class="number-term">931</span> = </span>
         </div>
       `,
     options: ["9265", "9245", "9285", "9305"],
@@ -119,7 +119,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>543 + 544 + 545 + 546 + 547 + 548 + 549 + 550 + 551 + 552 = </span>
+          <span><span class="number-term">543</span> + <span class="number-term">544</span> + <span class="number-term">545</span> + <span class="number-term">546</span> + <span class="number-term">547</span> + <span class="number-term">548</span> + <span class="number-term">549</span> + <span class="number-term">550</span> + <span class="number-term">551</span> + <span class="number-term">552</span> = </span>
         </div>
       `,
     options: ["5465", "5485", "5505", "5475"],
@@ -128,7 +128,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>274 + 275 + 276 + 277 + 278 + 279 + 280 + 281 + 282 + 283 = </span>
+          <span><span class="number-term">274</span> + <span class="number-term">275</span> + <span class="number-term">276</span> + <span class="number-term">277</span> + <span class="number-term">278</span> + <span class="number-term">279</span> + <span class="number-term">280</span> + <span class="number-term">281</span> + <span class="number-term">282</span> + <span class="number-term">283</span> = </span>
         </div>
       `,
     options: ["2765", "2785", "2805", "2825"],
@@ -137,7 +137,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>135 + 136 + 137 + 138 + 139 + 140 + 141 + 142 + 143 + 144 = </span>
+          <span><span class="number-term">135</span> + <span class="number-term">136</span> + <span class="number-term">137</span> + <span class="number-term">138</span> + <span class="number-term">139</span> + <span class="number-term">140</span> + <span class="number-term">141</span> + <span class="number-term">142</span> + <span class="number-term">143</span> + <span class="number-term">144</span> = </span>
         </div>
       `,
     options: ["1415", "1375", "1395", "1435"],
@@ -146,7 +146,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>326 + 327 + 328 + 329 + 330 + 331 + 332 + 333 + 334 + 335 = </span>
+          <span><span class="number-term">326</span> + <span class="number-term">327</span> + <span class="number-term">328</span> + <span class="number-term">329</span> + <span class="number-term">330</span> + <span class="number-term">331</span> + <span class="number-term">332</span> + <span class="number-term">333</span> + <span class="number-term">334</span> + <span class="number-term">335</span> = </span>
         </div>
       `,
     options: ["3275", "3295", "3305", "3315"],
@@ -155,7 +155,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>757 + 758 + 759 + 760 + 761 + 762 + 763 + 764 + 765 + 766 = </span>
+          <span><span class="number-term">757</span> + <span class="number-term">758</span> + <span class="number-term">759</span> + <span class="number-term">760</span> + <span class="number-term">761</span> + <span class="number-term">762</span> + <span class="number-term">763</span> + <span class="number-term">764</span> + <span class="number-term">765</span> + <span class="number-term">766</span> = </span>
         </div>
       `,
     options: ["7595", "7635", "7575", "7615"],
@@ -164,7 +164,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>918 + 919 + 920 + 921 + 922 + 923 + 924 + 925 + 926 + 927 = </span>
+          <span><span class="number-term">918</span> + <span class="number-term">919</span> + <span class="number-term">920</span> + <span class="number-term">921</span> + <span class="number-term">922</span> + <span class="number-term">923</span> + <span class="number-term">924</span> + <span class="number-term">925</span> + <span class="number-term">926</span> + <span class="number-term">927</span> = </span>
         </div>
       `,
     options: ["9215", "9235", "9225", "9195"],
@@ -173,7 +173,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>839 + 840 + 841 + 842 + 843 + 844 + 845 + 846 + 847 + 848 = </span>
+          <span><span class="number-term">839</span> + <span class="number-term">840</span> + <span class="number-term">841</span> + <span class="number-term">842</span> + <span class="number-term">843</span> + <span class="number-term">844</span> + <span class="number-term">845</span> + <span class="number-term">846</span> + <span class="number-term">847</span> + <span class="number-term">848</span> = </span>
         </div>
       `,
     options: ["8445", "8405", "8465", "8435"],
@@ -182,7 +182,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>260 + 261 + 262 + 263 + 264 + 265 + 266 + 267 + 268 + 269 = </span>
+          <span><span class="number-term">260</span> + <span class="number-term">261</span> + <span class="number-term">262</span> + <span class="number-term">263</span> + <span class="number-term">264</span> + <span class="number-term">265</span> + <span class="number-term">266</span> + <span class="number-term">267</span> + <span class="number-term">268</span> + <span class="number-term">269</span> = </span>
         </div>
       `,
     options: ["2645", "2625", "2665", "2605"],
@@ -191,7 +191,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>131 + 132 + 133 + 134 + 135 + 136 + 137 + 138 + 139 + 140 = </span>
+          <span><span class="number-term">131</span> + <span class="number-term">132</span> + <span class="number-term">133</span> + <span class="number-term">134</span> + <span class="number-term">135</span> + <span class="number-term">136</span> + <span class="number-term">137</span> + <span class="number-term">138</span> + <span class="number-term">139</span> + <span class="number-term">140</span> = </span>
         </div>
       `,
     options: ["1335", "1395", "1355", "1375"],
