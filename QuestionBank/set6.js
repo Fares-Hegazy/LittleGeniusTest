80e5faf27cc573f188e17c49248dcbcd0436cc01
Fares-Hegazy <mhegazy1522@gmail.com>
2025-08-02 09:04:33 +0300

    Create set6.js

---
+++ QuestionBank/set6.js
@@ -0,0 +1,12 @@
+export const set6 = [
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>736 + 492 + 815 + 367 + 921 + 583 + 674 + 228 + 459 + 876 = </span>
+          <span class="answer-blank">_____</span>
+        </div>
+      `,
+    options: ["5151", "5251", "5351", "5451"],
+    answer: "5151"
+  }
+  ];
