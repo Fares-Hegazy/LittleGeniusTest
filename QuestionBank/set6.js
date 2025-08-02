f87de1a6c320f42a6e882025dcd17c28ac6a721d
Fares-Hegazy <mhegazy1522@gmail.com>
2025-08-02 09:42:34 +0300

    Add files via upload

--- QuestionBank/set6.js
+++ QuestionBank/set6.js
@@ -2,11 +2,199 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>736 + 492 + 815 + 367 + 921 + 583 + 674 + 228 + 459 + 876 = </span>
+          <span>7 + 2 + 2 + 7 + 6 + 5 + 3 + 2 + 3 = </span>
         </div>
       `,
-    options: ["5151", "5251", "5351", "5451"],
-    answer: "5151"
+    options: ["47", "27", "37", "57"],
+    answer: "37"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>3 + 2 + 4 + 7 + 5 + 2 + 3 + 7 + 1 = </span>
+        </div>
+      `,
+    options: ["24", "34", "44", "54"],
+    answer: "34"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>3 + 6 + 3 + 1 + 2 + 4 + 6 + 8 + 5 = </span>
+        </div>
+      `,
+    options: ["38", "28", "48", "58"],
+    answer: "38"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>4 + 5 + 6 + 1 + 8 + 3 + 4 + 5 + 3 = </span>
+        </div>
+      `,
+    options: ["29", "49", "59", "39"],
+    answer: "39"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>3 + 2 + 5 + 8 + 1 + 5 + 7 + 1 + 7 = </span>
+        </div>
+      `,
+    options: ["29", "49", "59", "39"],
+    answer: "39"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>6 + 4 + 5 + 4 + 3 + 1 + 5 + 2 + 9 = </span>
+        </div>
+      `,
+    options: ["39", "49", "29", "59"],
+    answer: "39"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>5 + 2 + 6 + 2 + 3 + 5 + 4 + 3 + 5 = </span>
+        </div>
+      `,
+    options: ["55", "45", "35", "25"],
+    answer: "35"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>1 + 3 + 2 + 4 + 7 + 5 + 3 + 8 + 9 = </span>
+        </div>
+      `,
+    options: ["32", "42", "52", "62"],
+    answer: "42"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>8 + 3 + 2 + 2 + 3 + 5 + 7 + 3 + 2 = </span>
+        </div>
+      `,
+    options: ["35", "25", "45", "55"],
+    answer: "35"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>3 + 2 + 7 + 2 + 3 + 5 + 3 + 8 + 5 = </span>
+        </div>
+      `,
+    options: ["28", "48", "38", "58"],
+    answer: "38"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>2 + 7 + 3 + 8 + 5 + 2 + 9 + 1 + 2 = </span>
+        </div>
+      `,
+    options: ["29", "39", "49", "59"],
+    answer: "39"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>715 + 716 + 717 + 718 + 719 + 720 + 721 + 722 + 723 + 724 = </span>
+        </div>
+      `,
+    options: ["7175", "7195", "7215", "7235"],
+    answer: "7195"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>922 + 923 + 924 + 925 + 926 + 927 + 928 + 929 + 930 + 931 = </span>
+        </div>
+      `,
+    options: ["9265", "9245", "9285", "9305"],
+    answer: "9265"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>543 + 544 + 545 + 546 + 547 + 548 + 549 + 550 + 551 + 552 = </span>
+        </div>
+      `,
+    options: ["5465", "5485", "5505", "5475"],
+    answer: "5475"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>274 + 275 + 276 + 277 + 278 + 279 + 280 + 281 + 282 + 283 = </span>
+        </div>
+      `,
+    options: ["2765", "2785", "2805", "2825"],
+    answer: "2785"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>135 + 136 + 137 + 138 + 139 + 140 + 141 + 142 + 143 + 144 = </span>
+        </div>
+      `,
+    options: ["1415", "1375", "1395", "1435"],
+    answer: "1395"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>326 + 327 + 328 + 329 + 330 + 331 + 332 + 333 + 334 + 335 = </span>
+        </div>
+      `,
+    options: ["3275", "3295", "3305", "3315"],
+    answer: "3305"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>757 + 758 + 759 + 760 + 761 + 762 + 763 + 764 + 765 + 766 = </span>
+        </div>
+      `,
+    options: ["7595", "7635", "7575", "7615"],
+    answer: "7615"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>918 + 919 + 920 + 921 + 922 + 923 + 924 + 925 + 926 + 927 = </span>
+        </div>
+      `,
+    options: ["9215", "9235", "9225", "9195"],
+    answer: "9225"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>839 + 840 + 841 + 842 + 843 + 844 + 845 + 846 + 847 + 848 = </span>
+        </div>
+      `,
+    options: ["8445", "8405", "8465", "8435"],
+    answer: "8435"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>260 + 261 + 262 + 263 + 264 + 265 + 266 + 267 + 268 + 269 = </span>
+        </div>
+      `,
+    options: ["2645", "2625", "2665", "2605"],
+    answer: "2645"
+  },
+  {
+    question: `
+        <div class="horizontal-question">
+          <span>131 + 132 + 133 + 134 + 135 + 136 + 137 + 138 + 139 + 140 = </span>
+        </div>
+      `,
+    options: ["1335", "1395", "1355", "1375"],
+    answer: "1355"
   }
-  ];
-//update
+];
