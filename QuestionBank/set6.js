3fc0ee909e49e0e04e36cdc17d8e01fc45199669
Fares-Hegazy <mhegazy1522@gmail.com>
2025-08-02 09:15:16 +0300

    Update set6.js

--- QuestionBank/set6.js
+++ QuestionBank/set6.js
@@ -2,7 +2,7 @@ export const set6 = [
   {
     question: `
         <div class="horizontal-question">
-          <span>736 + 492 + 815 + 367 + 921 + 583 + 674 + 228 + 459 + 876 = </span>
+          <span>736 + 492 + 815 + 367 + 921 + 583 + 674 + 228 + 459 + 876 = ...</span>
         </div>
       `,
     options: ["5151", "5251", "5351", "5451"],
