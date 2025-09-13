afd316120eadd958e89a22a93904d58d5834a916
Fares-Hegazy <mhegazy1522@gmail.com>
2025-09-13 20:01:05 +0300

    Add files via upload

---
+++ QuestionBank/set7.js
@@ -0,0 +1,277 @@
+export const set7 = [
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  944</div>
+          <div class="row"><span class="op">-</span>  526</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["418", "436", "509", "327"],
+    answer: "418"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  945</div>
+          <div class="row"><span class="op">-</span>  529</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["305", "416", "427", "614"],
+    answer: "416"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  645</div>
+          <div class="row"><span class="op">-</span>  436</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["209", "415", "201", "308"],
+    answer: "209"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  968</div>
+          <div class="row"><span class="op">-</span>  326</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["523", "642", "619", "741"],
+    answer: "642"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  876</div>
+          <div class="row"><span class="op">-</span>  549</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["208", "317", "327", "429"],
+    answer: "327"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  9569</div>
+          <div class="row"><span class="op">-</span>  5293</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["3184", "4276", "4062", "2391"],
+    answer: "4276"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  5864</div>
+          <div class="row"><span class="op">-</span>  3549</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["2315", "2074", "1426", "2917"],
+    answer: "2315"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  5653</div>
+          <div class="row"><span class="op">-</span>  3565</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["2974", "2088", "2014", "1089"],
+    answer: "2088"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  4663</div>
+          <div class="row"><span class="op">-</span>  1298</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["5093", "2471", "3365", "3082"],
+    answer: "3365"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  8534</div>
+          <div class="row"><span class="op">-</span>  5389</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["4281", "3067", "3145", "1359"],
+    answer: "3145"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  66496</div>
+          <div class="row"><span class="op">-</span>  23639</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["42857", "30762", "41973", "40124"],
+    answer: "42857"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  52684</div>
+          <div class="row"><span class="op">-</span>  32826</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["29741", "19858", "10632", "31509"],
+    answer: "19858"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  36563</div>
+          <div class="row"><span class="op">-</span>  22836</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["14729", "13727", "10918", "30514"],
+    answer: "13727"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  54245</div>
+          <div class="row"><span class="op">-</span>  28129</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["20827", "15294", "26116", "37905"],
+    answer: "26116"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  65284</div>
+          <div class="row"><span class="op">-</span>  38139</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["27145", "20917", "18326", "42903"],
+    answer: "27145"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  682</div>
+          <div class="row"><span class="op">-</span>  287</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["503", "395", "307", "194"],
+    answer: "395"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  964</div>
+          <div class="row"><span class="op">-</span>  269</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["413", "607", "695", "626"],
+    answer: "695"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  815</div>
+          <div class="row"><span class="op">-</span>  418</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["397", "304", "619", "297"],
+    answer: "397"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  536</div>
+          <div class="row"><span class="op">-</span>  237</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["409", "299", "217", "517"],
+    answer: "299"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  867</div>
+          <div class="row"><span class="op">-</span>  669</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["412", "198", "105", "527"],
+    answer: "198"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  945</div>
+          <div class="row"><span class="op">-</span>  544</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["401", "308", "414", "501"],
+    answer: "401"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  489</div>
+          <div class="row"><span class="op">-</span>  288</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["403", "107", "201", "291"],
+    answer: "201"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  656</div>
+          <div class="row"><span class="op">-</span>  353</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["303", "412", "396", "507"],
+    answer: "303"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  688</div>
+          <div class="row"><span class="op">-</span>  384</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["206", "409", "304", "317"],
+    answer: "304"
+  },
+  {
+    question: `
+        <div class="vertical-question">
+          <div class="row">  429</div>
+          <div class="row"><span class="op">-</span>  322</div>
+          <div class="line">_____</div>
+        </div>
+      `,
+    options: ["305", "107", "116", "208"],
+    answer: "107"
+  }
+];
